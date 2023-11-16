5e1f5a5b495b8035c0906ea6edf783477cb80ecd
ali <alirahmanicode@gmail.com>
2023-11-16 17:54:32 +0330

    create costum user

--- src/drive/settings.py
+++ src/drive/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local apps
+    "drive.auth.users",
 ]
 
 MIDDLEWARE = [
@@ -80,6 +82,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "users.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
