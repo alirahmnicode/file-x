2dd6804e8377b22e4ed707d31e7ac2530478f1d1
ali <alirahmanicode@gmail.com>
2023-11-17 17:16:17 +0330

    update

--- src/drive/urls.py
+++ src/drive/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static 
+from django.conf import settings 
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -25,3 +27,4 @@ urlpatterns = [
 ]
 
 urlpatterns += doc_patterns
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
