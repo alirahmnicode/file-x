3af2a426f1a8b2fea55826f7d41a7da8dc22bdc0
ali <alirahmanicode@gmail.com>
2023-11-17 17:00:46 +0330

    Update

--- src/drive/urls.py
+++ src/drive/urls.py
@@ -1,21 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework.authtoken import views
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
+    SpectacularJSONAPIView,
 )
+
+from .views import index
 
 
 doc_patterns = [
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/yaml/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/json/', SpectacularJSONAPIView.as_view(), name='json-schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path("users/", include("users.urls")),
 ]
