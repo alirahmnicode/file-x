54315d60e1bc3b5821bbaf511ecba101672e1ee1
ali <alirahmanicode@gmail.com>
2023-11-17 12:49:44 +0330

    update:

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import LoginAPIView, Signup, Test
+from .views import LoginAPIView, Signup
 
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view()),
     path('signup/', Signup.as_view()),
-    path('test/', Test.as_view()),
 ]
