7a729565d834179589cb7383bb3a2b030c23ea19
ali <alirahmanicode@gmail.com>
2023-11-19 11:08:33 +0330

    update

--- src/apps/multimedia/serializers.py
+++ src/apps/multimedia/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
-from apps.multimedia.models import File
-
+from apps.multimedia.models import File as FileModel
 
 
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = File
-        fields = ("file",)
+        model = FileModel
+        fields = ("id", "file")
+
+
+class UploadMultipleFileSerializer(serializers.Serializer):
+
+    files = serializers.ListField(write_only=True,
+        child=serializers.FileField(allow_empty_file=False, use_url=False)
+    )
+
+
+    def create(self, validated_data):
+        files = validated_data.get("files")
+        user = self.context.get("request").user
+        files_list = []
+        
+        files_list = [FileModel(file=file, user=user) for file in files]    
+
+        created_file = FileModel.objects.bulk_create(files_list)
+        return created_file
--- src/apps/multimedia/views.py
+++ src/apps/multimedia/views.py
@@ -1,15 +1,31 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from apps.multimedia.models import File
-from apps.multimedia.serializers import FileSerializer
+from apps.multimedia.serializers import UploadMultipleFileSerializer, FileSerializer
 
 
-class FileViewSet(ModelViewSet):
+class CustomFileModelViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    pass
+
+
+class FileViewSet(CustomFileModelViewSet):
     queryset = File.objects.all()
-    serializer_class = FileSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return UploadMultipleFileSerializer
+        else:
+            return FileSerializer
