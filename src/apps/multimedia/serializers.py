5e797b52c4a92acef15aa6c8c944720b217fba42
ali <alirahmanicode@gmail.com>
2023-11-25 19:22:27 +0330

    update

--- src/apps/multimedia/serializers.py
+++ src/apps/multimedia/serializers.py
@@ -4,13 +4,25 @@ from apps.multimedia.models import File as FileModel
 
 class FileSerializer(serializers.ModelSerializer):
     file_format = serializers.SerializerMethodField("get_file_format")
+    file_name = serializers.SerializerMethodField("get_file_name")
+    file_size = serializers.SerializerMethodField("get_file_size")
 
     def get_file_format(self, obj):
         return obj.file.name.split(".")[-1]
 
+    def get_file_name(self, obj):
+        return obj.file.name.split("/")[-1]
+
+    def get_file_size(self, obj):
+        size = obj.file.size
+        if size < 1_000_000:
+            return f"{size / 1_000}kb"
+        else:
+            return f"{size / 1_000_000}mb"
+
     class Meta:
         model = FileModel
-        fields = ("id", "file", "file_format")
+        fields = ("id", "file", "file_format", "file_name", "file_size")
 
 
 class UploadMultipleFileSerializer(serializers.Serializer):
